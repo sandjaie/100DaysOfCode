31a5f4f66e746d45f9e05753d079b55c82b788f9
sandjaie <sandjaie@gmail.com>
2019-07-06 18:31:45 +0530

    Added functions

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,22 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
-
-url = "https://www.amazon.in/s?k=oneplus+7&ref=nb_sb_noss"
-
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+from config import *
 
 page = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-prices_text = [ f'"{price.get_text()}"' for price in soup.find_all('span', {'class': 'a-price-whole'}) ]
-titles_text = [ f'"{title.get_text()}"' for title in soup.find_all('span', {'class': 'a-size-medium a-color-base a-text-normal'}) ]
-serial_numbers = [ str(serial_number) for serial_number  in range(1, len(prices_text))]
-zipped_items = zip(serial_numbers, prices_text, titles_text)
+def zip_items():
+    """Gets the price list and titles list and zips them into one single list
+    
+    Returns:
+        zippered_items [list] -- [list of serial_number, price, title]
+    """
+    try:
+        prices_list = [ f'"{price.get_text()}"' for price in soup.find_all('span', {'class': 'a-price-whole'}) ]
+        titles_list = [ f'"{title.get_text()}"' for title in soup.find_all('span', {'class': 'a-size-medium a-color-base a-text-normal'}) ]
+        serial_numbers = [ str(serial_number) for serial_number in range(1, len(prices_list))]
+        zipped_items = zip(serial_numbers, prices_list, titles_list)
+        return zipped_items
+    except Exception as err:
+        return err
 
-csv_headers = ['Serial Number', 'Price', 'Product']
+def write_file():
+    """Writes the data into a csv file
+    """
+    csv_headers = ['Serial Number', 'Price', 'Product']
+    zipped_items = zip_items()
+    try:
+        with open('products.csv', 'w') as f:
+            f.write(','.join(csv_headers) + '\n')
+            for item in zipped_items:
+                f.write(','.join(item) + '\n')
+        print("products.csv is ready")
 
-with open('products.csv', 'w') as f:
-    f.write(','.join(csv_headers) + '\n')
-    for item in zipped_items:
-        f.write(','.join(item) + '\n')
+    except Exception as err:
+        return err
+    
+if __name__ == "__main__":
+    write_file()
