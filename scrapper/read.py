613917fe3e93dcf273d1384e58d4be8a73d88471
sandjaie <sandjaie@gmail.com>
2019-07-12 14:34:48 +0530

    edited main fn

--- scrapper/read.py
+++ scrapper/read.py
@@ -1,14 +1,14 @@
 import csv
 import json
 
-#Read the file, not used anywhere.
+#Reads the file, not used anywhere.
 def read_text():
     """Prints the file
     """
-    with open('products.csv', 'r') as r:
-        line = r.readlines()
-        for l in line:
-            print(l, end='\n')
+    with open('products.csv', 'r') as file_pointer:
+        lines = file_pointer.readlines()
+        for line in lines:
+            print(line, end='\n')
 
 def read_json_data():
     """Returns the json file
@@ -17,25 +17,25 @@ def read_json_data():
 
 def find_max_priced_product():
     """Sort the json file and return the max priced product
-    
+
     Returns:
         product [list] -- [max priced product]
     """
     data = read_json_data()
-    products = sorted(data, key=lambda product: int(product['Price'].replace(',','')), reverse=True)
+    products = sorted(data, key=lambda product: int(product['Price'].replace(',', '')), reverse=True)
     return products[0]
 
 def get_price():
     """Get the price of all the products
-    
+
     Returns:
         price_list [list] -- [price_list]
     """
-    with open('products.csv', 'r') as f:
-        f = csv.reader(f, delimiter=',')
+    with open('products.csv', 'r') as fp:
+        fp = csv.reader(fp, delimiter=',')
         price_list = []
-        for row in f:
-            row[1] = row[1].replace(',','')
+        for row in fp:
+            row[1] = row[1].replace(',', '')
             if row[1].isdigit():
                 price_list.append(int(row[1]))
     return price_list
@@ -53,18 +53,21 @@ def find_max_price():
 #Not used anywhere
 def find_max_priced_product_csv():
     """Returns max priced product name and price
-    
+
     Returns:
         product [list] -- [returns high priced product as a list]
     """
-    with open('products.csv', 'r') as f:
-        f = list(csv.reader(f, delimiter=','))
-        products = sorted(f[1:], key=lambda item: int(item[1].replace(',','')), reverse=True)
+    with open('products.csv', 'r') as fp:
+        fp = list(csv.reader(fp, delimiter=','))
+        products = sorted(fp[1:], key=lambda item: int(item[1].replace(',', '')), reverse=True)
     return products[0]
 
-#Not used anywhere
 def print_max_priced_product():
+    """Prints the max priced product
+    """
     print("Max Price from the list:", find_max_price())
-    print(f"Price: {find_max_priced_product()[1]}")
-    print(f"Product: {find_max_priced_product()[2]}")
+    print(f"Price: {find_max_priced_product_csv()[1]}")
+    print(f"Product: {find_max_priced_product_csv()[2]}")
 
+if __name__ == '__main__':
+    print_max_priced_product()
